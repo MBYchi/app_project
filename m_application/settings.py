d07aa91e08bca93ac2e6b09ba1e35a446bad2adc
meep00 <walentylambin@gmail.com>
2024-11-23 20:24:08 +0100

    mysql container added

--- m_application/settings.py
+++ m_application/settings.py
@@ -15,11 +15,11 @@ import os
 from dotenv import load_dotenv
 import m_application
 
-load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(str(BASE_DIR)+'/.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
